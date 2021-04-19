a7acb1d6fcf8e37b4006fe42b71e4d51d2cbb792
AndreasIrta <andreasirta0711@gmail.com>
2021-04-18 22:10:06 -0600

    add question list

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,39 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require("fs");
 const license = ["MIT", "GPL", "APACHE"]
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Enter Project Title"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter Project Description"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose License",
+        choice: license
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter Usage Information"
+    },
+    {
+        type: "input",
+        name: "guideline",
+        message: "Enter contribution Guidelines"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Enter Test Instructions"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
